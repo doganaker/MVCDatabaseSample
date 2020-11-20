da946c7f1233c52be88a92ffe2f72427791fa8c9
Doğan Aker <m.doganaker@gmail.com>
2020-11-20 18:57:21 +0300

    ProductsCategoriesSuppliers

---
+++ Proje2/Models/ORM/Context/TradeNetworkContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proje2.Models.ORM.Context
+{
+    public class TradeNetworkContext : DbContext
+    {
+        public TradeNetworkContext(DbContextOptions<TradeNetworkContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Proje2/Models/ORM/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proje2.Models.ORM.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string Descripiton { get; set; }
+        public dynamic Picture { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
