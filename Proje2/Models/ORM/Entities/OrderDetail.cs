eac102f80118838ffea7efacbee49b7ae9d10fca
Hande Alaca <69791848+handealaca@users.noreply.github.com>
2020-11-21 20:09:58 +0300

    Order and OrderDetail Entities Added

---
+++ Proje2/Models/ORM/Entities/OrderDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proje2.Models.ORM.Entities
+{
+    public class OrderDetail
+    {
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public dynamic Discount { get; set; }
+    }
+}
